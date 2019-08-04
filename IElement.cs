4c167e93d63ec6a1e34daf9caa32a7577762a75b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-08-04 23:17:13 +0900

    判別要素の強さを表すインタフェースを定義。

---
+++ IElement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlotSettingDiscriminationFramework
+{
+	/// <summary>
+	/// 判別要素インタフェース
+	/// </summary>
+	public interface IElement
+	{
+		/// <summary>
+		/// 要素名
+		/// </summary>
+		string Name { get; }
+
+		/// <summary>
+		/// 判別要素の強さ
+		/// </summary>
+		int Weight { get; }
+	}
+}
